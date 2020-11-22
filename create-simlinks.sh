b61b49f149b1bbcfb78486cb261883d319ada425
bleart nuredini <bleart@gmail.com>
2020-11-22 23:02:32 +0100

    updated the install script

--- create-simlinks.sh
+++ create-simlinks.sh
@@ -4,3 +4,4 @@ ln -sfv ~/.dotfiles/.vimrc ~/.vimrc
 ln -sfv ~/.dotfiles/.bashrc ~/.bashrc
 ln -sfv ~/.dotfiles/.bash_profile ~/.bash_profile 
 ln -sfv ~/.dotfiles/.bash_prompt ~/.bash_prompt
+ln -sfv ~/.dotfiles/.aliases ~/.aliases
