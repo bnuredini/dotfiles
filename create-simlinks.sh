4042a9a0cc548a6822308e8654377ebc369a3a43
bleart nuredini <nuredini.bleart@gmail.com>
2021-03-24 22:23:18 +0100

    add i3 config file

--- create-simlinks.sh
+++ create-simlinks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ln -sfv ~/.dotfiles/.vimrc ~/.vimrc
-# ln -sfv ~/.dotfiles/config ~/.config/i3/config
+ln -sfv ~/.dotfiles/config ~/.config/i3/config
 ln -sfv ~/.dotfiles/.bashrc ~/.bashrc
 ln -sfv ~/.dotfiles/.bash_profile ~/.bash_profile 
 ln -sfv ~/.dotfiles/.bash_prompt ~/.bash_prompt
