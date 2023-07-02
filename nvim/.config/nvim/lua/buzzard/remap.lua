5c0dd58d2c5bc828b04466052bb7451e6706941a
Bleart Nuredini <nuredini.bleart@gmail.com>
2023-07-02 15:02:44 +0200

    nvim: add remapping for funciton keys

--- nvim/.config/nvim/lua/buzzard/remap.lua
+++ nvim/.config/nvim/lua/buzzard/remap.lua
@@ -26,6 +26,20 @@ vim.keymap.set("n", "sh", ":split<CR>")
 
 vim.keymap.set("n", "\\s", ":setlocal spell! spelllang=en_us<CR>")
 
+vim.keymap.set("n", "<F2>", ":%s/w/ë/g<CR>")
+vim.keymap.set("n", "<F3>", ":%s/@/ç/g<CR>")
+vim.keymap.set("n", "<F4>", ":%s/&/w/g<CR>")
+vim.keymap.set("n", "<F9>", ":%s/W/Ë/g<CR>")
+vim.keymap.set("n", "<F10>", ":w<CR>:VimwikiAll2HTML<CR>")
+
+vim.keymap.set("n", "<leader>f", vim.cmd.Goyo)
+vim.keymap.set("n", "<leader>n", vim.cmd.tabnew)
+vim.keymap.set("n", "<leader>w", vim.cmd.tabclose)
+vim.keymap.set("n", "<leader>b", ":Fern .<CR>")
+
+vim.keymap.set("n", "<leader>ts", "m'A<C-R>=strftime('%Y%m%dT%H%M%S')<CR>")
+vim.keymap.set("n", "<leader>td", "a### <C-R>=strftime('%Y-%m-%d')<CR>")
+
 vim.cmd([[
     autocmd FileType javascript setlocal tabstop=2
     autocmd FileType javascript setlocal shiftwidth=2
@@ -34,4 +48,23 @@ vim.cmd([[
     autocmd FileType typescript setlocal tabstop=2
     autocmd FileType typescript setlocal shiftwidth=2
     autocmd FileType typescript setlocal softtabstop=2
+
+    autocmd Filetype css setlocal tabstop=2
+    autocmd Filetype css setlocal shiftwidth=2
+
+    autocmd Filetype php setlocal tabstop=2
+    autocmd Filetype php setlocal shiftwidth=2
 ]])
+
+vim.cmd([[
+  autocmd FileType java nmap <F7> :w<CR>:!javac % && java -cp %:p:h %:t:r<CR>
+]])
+
+vim.cmd([[
+  autocmd Filetype c nmap <F7> :w<CR>:!gcc % && ./a.out<CR>
+]])
+
+vim.cmd([[
+  autocmd Filetype sh nmap <F7>:w<CR>!./%<CR>
+]])
+
