620c43c06c731450596ffbd1e113135979b4738b
Bleart Nuredini <nuredini.bleart@gmail.com>
2025-02-13 23:55:34 +0100

    wezterm: update keybindings

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,18 +1,84 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font_size = 12.0
+config.font_size = 9.0
 config.window_decorations = "RESIZE"
-config.color_scheme = "Jellybeans"
-config.colors = {
-	background = "#151515",
-}
+config.font = wezterm.font("JetBrains Mono")
+
+local dark_mode = function()
+	config.color_scheme = "Jellybeans"
+	config.colors = {
+		background = "#151515",
+	}
+end
+
+local light_mode = function()
+	config.color_scheme = "AtomOneLight"
+end
+
+dark_mode()
+-- light_mode()
 
 config.keys = {
 	{
 		key = '"',
 		mods = "CTRL|SHIFT",
 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "RightArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "UpArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "[",
+		mods = "CTRL",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = "]",
+		mods = "CTRL",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "o",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivateLastTab,
 	},
 }
 
