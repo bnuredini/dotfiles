7eaab4c343952ebbd7168dd5f0fc1444bd548a13
Bleart Nuredini <nuredini.bleart@gmail.com>
2025-10-15 17:30:34 +0200

    fish: remove old aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -36,33 +36,25 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../..."
 
-# tmp
-alias main='javac Main.java && java Main'
-alias vw='cd ~/documents/notes && vim index.wiki'
-alias code="codium"
-alias p="python3"
-
 # scripts
-alias setupmonitor='~/code/shell/setup_monitor.sh'
-alias unsetupmonitor='~/code/shell/unsetup_monitor.sh'
-alias rleft='~/code/shell/rotate_left_monitor.sh'
-alias rnormal='~/code/shell/rotate_normal_monitor.sh'
+alias monitor-setup='~/code/scripts/setup_monitor.sh'
+alias monitor-unsetup='~/code/scripts/unsetup_monitor.sh'
+alias monitor-rotate-left='~/code/scripts/rotate_left_monitor.sh'
+alias monitor-rotate-right='~/code/scripts/rotate_normal_monitor.sh'
 
-# ls aliases
+# other programs
+alias v='nvim'
+alias vf='nvim (fzf)'
+alias p="python3"
 alias ll='exa -alF'
 alias la='exa -A'
 alias l='exa -CF'
 alias lsj='exa | grep .java'
 
-# my aliases
-alias v='nvim'
-alias vf='nvim (fzf)'
-alias vim='nvim'
+# documents
 alias td='cd ~/documents && nvim ~/documents/todo.md'
-alias photos='sxiv -tf ~/media/photos/100andro/'
-alias wp='sxiv -tf ~/media/wallpapers/'
-alias jodoro='java -cp ~/Projects/jodoro/target/classes outer.space.jodoro.GUI&'
-# alias o='xdg-open (fzf)'
+alias vw='cd ~/documents/notes && vim index.wiki'
+alias opend="nvim ~/documents/(date +'%Y-%m-%d').md"
 
 # config files
 alias vconf='cd ~/.config/nvim/ && nvim ~/.config/nvim/init.lua'
@@ -70,14 +62,6 @@ alias i3conf='nvim ~/.config/i3/config'
 alias pconf='nvim ~/.config/polybar/config'
 alias lfconf='nvim ~/.config/lf/lfrc'
 alias fconf='nvim ~/.config/fish/config.fish'
-
-alias rmc='rm *.class'
-alias rmo='rm -rf *.out*'
-alias rmt='rm -rvi ./tmp'
-alias r='ranger'
-alias z='fzf --bind "enter:execute(swallow zathura {}&)"'
-alias zb='cd ~/books && fzf --bind "enter:execute(swallow zathura {}&)"'
-alias date2name='~/downloads/bin/date2name/date2name/date2name.py'
 
 # volume aliases
 alias am='amixer set Master 100'	# set max
